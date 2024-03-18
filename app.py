e769e3bf7f17f5fe50b9dae4c58a26570587c66f
Satish <Aishwarya.Satish@team.telstra.com>
2024-03-18 20:49:55 +0530

    mounting efs'

--- app.py
+++ app.py
@@ -1,28 +1,11 @@
-#!/usr/bin/env python3
-import os
-
+from aws_cdk import core
 import aws_cdk as cdk
-
 from ec2.ec2_stack import Ec2Stack
 
+app = core.App()
 
-app = cdk.App()
-Ec2Stack(app, "Ec2Stack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+Ec2Stack(app, "Ec2Stack", env=cdk.Environment(account='851725626130', region='us-east-1'))  # Pass the environment as an argument
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
-
+# Synthesize the CloudFormation template
 app.synth()
--- ec2/ec2_stack.py
+++ ec2/ec2_stack.py
@@ -1,19 +1,51 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
-from constructs import Construct
+from aws_cdk import core, aws_ec2 as ec2, aws_efs as efs
 
-class Ec2Stack(Stack):
+class Ec2Stack(core.Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create VPC
+        vpc = ec2.Vpc(self, "MyVpc",
+            cidr="10.0.0.0/16",
+            max_azs=2
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "Ec2Queue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create EFS file system
+        efs_sg = ec2.SecurityGroup(self, "EfsSecurityGroup",
+            vpc=vpc,
+            allow_all_outbound=True  
+        )
+
+        efs_fs = efs.FileSystem(self, "MyEFS",
+            vpc=vpc,
+            encrypted=True,
+            security_group=efs_sg 
+        )
+
+        # Create EC2 instance
+        instance_sg = ec2.SecurityGroup(self, "InstanceSecurityGroup",
+            vpc=vpc,
+            allow_all_outbound=True  
+        )
+
+        instance = ec2.Instance(self, "MyInstance",
+            instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+            machine_image=ec2.MachineImage.latest_amazon_linux(),
+            vpc=vpc,
+            security_group=instance_sg  
+        )
+
+       
+        efs_sg.add_ingress_rule(
+            peer=instance_sg,
+            connection=ec2.Port.tcp(2049),
+            description="Allow NFS traffic from EC2 instance"
+        )
+
+        # Mount EFS to EC2 instance
+        instance.user_data.add_commands(
+            "yum install -y amazon-efs-utils",
+            f"mkdir -p /mnt/efs",
+            f"mount -t efs {efs_fs.file_system_id}:/ /mnt/efs"
+        )
